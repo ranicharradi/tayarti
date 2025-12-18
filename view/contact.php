7c9c3fbd94192e65ef8b8378e59a3e4db3905829
ranicharradi <charradirani@gmail.com>
2025-12-18 12:04:28 +0100

    Cleanup: remove leftovers and fix assets

--- view/contact.php
+++ view/contact.php
@@ -2,19 +2,18 @@
 include('../model/usersclass.php');
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="../public/css/style 2.css">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="../public/css/style-2.css">
 </head>
 
 <body>
 	<?php
 	include('navbar.php');
-	include('header.php');
-
 	?>
 	<section class="ftco-section">
 		<div class="container">
